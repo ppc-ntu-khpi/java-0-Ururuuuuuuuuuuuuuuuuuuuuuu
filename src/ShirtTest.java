21607aa9a9ae5c0dcc9779d390d175220d5a3128
Ururuuuuuuuuuuuuuuuuuuuuuu <kushkovova81@gmail.com>
2023-02-28 13:40:17 +0200

    ішвгіавшріащшоращащріарщшавішгаітовіа

--- src/ShirtTest.java
+++ src/ShirtTest.java
@@ -1,7 +1,7 @@
 public class ShirtTest {
 
     public static void main(String args[]) {
-        Shirt myShirt = new Shirt();
+        Shirt myShirt = new Shirt(1, "РУБАХА", "Black", 100, 10);
         myShirt.displayShirtInformation();
     }
 }
